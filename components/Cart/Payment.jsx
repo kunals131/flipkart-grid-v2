a8ae19250e18472da2fea72ca90c3bb4a7621e2b
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-07-31 22:02:05 +0530

    fixed payments loading

--- components/Cart/Payment.jsx
+++ components/Cart/Payment.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { placeOrder } from '../../store/order/actions';
 import Input from '../Input'
 import {useMoralis} from 'react-moralis'
-import {useNotification} from 'web3uikit'
+import {Loading, Skeleton, useNotification} from 'web3uikit'
 
 const Payment = ({step,setStep,form,setForm}) => {
+  const [dimenstion,setDimenstion] = useState({
+    height : '500',
+    width : '600'
+  })
   const handleChange = (e)=>{
     setForm((prev)=>({...prev,[e.target.name] : e.target.value}));
 }
+
+
 const {isWeb3Enabled,isWeb3EnableLoading,account,chainId} = useMoralis();
 const dispatch = useDispatch();
+
+const {loading} = useSelector(state=>state.orders)
+
 const notificationDispatch = useNotification();
 const handlePlaceOrder = ()=>{
   if (isWeb3Enabled) {
@@ -25,8 +34,30 @@ const handlePlaceOrder = ()=>{
     })
   }
 }
+
+useEffect(()=>{
+  setTimeout(()=>{
+    setDimenstion({
+      height : ref.current.clientHeight,
+      width : ref.current.clientWidth
+    })
+  }, 500)
+}, [])
+
+const ref = useRef();
+
+if (loading) return <div className='flex items-center justify-center' style={{height : dimenstion.height, width:  dimenstion.width}}>
+<Loading
+    size={50}
+    spinnerColor="#2E7DAF"
+    text='Placing your order....'
+  />
+ 
+  </div>
+
+
   return (
-    <div>
+    <div ref={ref}>
        <div className='text-2xl font-[500] '>Apply Coupon</div>
        <div className='mt-6 grid grid-cols-[2.7fr_1fr] gap-5'>
         <Input value={form.couponCode} name="couponCode" onChange={handleChange} label='Coupon Code'/>
